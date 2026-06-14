1bdaa301fcc4e4eb7c244701c797310584aef9c8
Nico Barrera <57334167+nicodsd@users.noreply.github.com>
2026-06-14 16:18:46 -0300

    Update Content Security Policy for styles and connections

--- proxy.ts
+++ proxy.ts
@@ -12,14 +12,14 @@ export function proxy(req: NextRequest) {
     const cspHeader = `
         default-src 'self';
         script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ""};
-        style-src 'self' ${isDev ? "'unsafe-inline'" : `'nonce-${nonce}'`};
+        style-src 'self' 'nonce-${nonce}' 'unsafe-hashes';
         img-src 'self' blob: data: https://res.cloudinary.com https://cdn.pixabay.com https://asset.cloudinary.com;
         font-src 'self';
         connect-src 'self' 
             ${process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"} 
             http://qmenu.digital 
             https://qmenu.digital 
-            https://api.emailjs.com; // <--- AGREGADO AQUÍ
+            https://api.emailjs.com;
         object-src 'none';
         base-uri 'self';
         form-action 'self';
